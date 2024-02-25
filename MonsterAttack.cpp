b9e783d8eb1682140aeae88565b5950246b1190c
Atharva Rakshak <atharvarakshak23@gmail.com>
2024-02-25 23:29:01 +0530

    25/2/24

--- MonsterAttack.cpp
+++ MonsterAttack.cpp
@@ -1,40 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    map<int,int> mp;
+    vector<int> a(n),x(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    } 
+    for(int i=0;i<n;i++){
+        cin>>x[i];
+    } 
+    for(int i=0;i<n;i++){
+        mp[abs(x[i])]+=a[i];
+    } 
+    vector<pair<int,int>> v;
+    for(auto i : mp){
+        v.push_back({i.first,i.second});
+    }
+    int index =0;
+    for(int i=0;i<n;i++){
+        if(index>=v.size()){
+            cout<<"YES"<<endl;
+            return;
+        }
+        if(v[index].first <= i){
+            cout<<"NO"<<endl;
+            return;
+            
+        }
+        int curr=k;
+        while(index<v.size() && curr>0){
+            if(v[index].second > curr){
+                v[index].second-=curr;
+                break;
+            }
+            else{
+                curr-=v[index].second;
+                v[index].second=0;
+                index++;
+            }
+        }
+    }
+    if(v.back().second>0){
+        cout<<"NO"<<endl;
+            return; 
+    }
+    cout<<"YES"<<endl;
+    return;
+}
 int main()
 {
-    int t, n,k;
+    int t;
     cin >> t;
     while (t--)
     {
-        cin >> n>>k;
-        long long s = 0,d=0;
-        vector<int> a(n),x(n),possible(n+1,0);
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-            s+=a[i];
-        }
-        for(int i=0;i<n;i++){
-            cin>>x[i];
-           possible[abs(x[i])]+=a[i];
-        }
-        int bullets=k;
-        bool b = 1;
-        for(int i=1;i<=n;i++){
-            if(bullets<possible[i]){
-                b=0;
-                break;
-            }
-            bullets-=possible[i];
-            bullets+=k;
-        }
-  
-        if( b==1){
-            cout<<"YES"<<endl;
-        }
-        else{
-
-            cout<<"NO"<<endl;
-        }
-
+        solve();
     }
 }
