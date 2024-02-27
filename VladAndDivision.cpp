c26c8764fb961b82e801dfcb20d4030dcf010c12
Atharva Rakshak <atharvarakshak23@gmail.com>
2024-02-27 23:09:17 +0530

    27/2/24

--- VladAndDivision.cpp
+++ VladAndDivision.cpp
@@ -21,15 +21,36 @@ int main()
         cin>>n;
         map<string,long long> mp;
         vector<long long> a(n);
-        cin>>a;
+        // cin>>a;
         for(int i=0;i<n;i++){
             cin>>a[i];
+            string bits = numTobits(a[i]);
+            mp[bits]++;
           
         }
         long long ans = n;
         for(int i=0;i<n;i++){
                 long long x = a[i];
+            string flipbits="";
+            string bits= numTobits(a[i]);
+            if(mp[bits]==0)continue;
+            while(x>0){
+                char bit = '0'+(x%2);
+                if(bit == '0'){
+                    flipbits+="1";
+                }
+                else flipbits+="0";
+                x/=2;
+
+            }
+            while(flipbits.size()<31)flipbits+="1";
+            if(mp[flipbits]>0){
+                mp[flipbits]--;
+                mp[bits]--;
+                ans--;
+            }
         }
+        cout<<ans<<endl;
 
 
     }
