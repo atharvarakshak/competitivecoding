e555e618b83397cb4e95f6d5b96e56c4cbca8d81
Atharva Rakshak <atharvarakshak23@gmail.com>
2025-07-08 00:03:52 +0530

    7/7/25

---
+++ C_Make_It_Beautiful.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+  return 0;  
+}
+
+
+int main()
+{
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    solve();
+  }
+
+}
